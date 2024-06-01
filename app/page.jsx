a6021cb25958190baf7c163f70479d98406dad9e
cesaltinofelix <cesaltinofelix2000@gmail.com>
2024-06-01 14:29:08 +0100

    updating social links

--- app/page.jsx
+++ app/page.jsx
@@ -27,7 +27,12 @@ const Home = () => {
               <FiDownload className="text-xl"/>
             </Button>
             <div className="mb-8 xl:mb-0">
-              <Socials/>
+              
+              <Socials 
+              containerStyles="flex gap-6" 
+              iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+              />
+
             </div>
           </div>
         </div>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,20 +1,25 @@
-import { Link } from "lucide-react"
-import {FaGitHub, FaLinkedinIn, FaYoutube, FaTwitter} from "react-icons/fa"
+
+import Link from "next/link"
+import {FaGithub, FaLinkedinIn, FaYoutube, FaTwitter} from "react-icons/fa"
 
 
 const socials = [
-    {icon: <FaGitHub/>, path: "https://github.com/CesaltinoFelix"},
+    {icon: <FaGithub/>, path: "https://github.com/CesaltinoFelix"},
     {icon: <FaLinkedinIn/>, path: "https://www.linkedin.com/in/cesaltinofelix"},
     {icon: <FaYoutube/>, path: "https://www.youtube.com/@cesaltinofelix"},
     {icon: <FaTwitter/>, path: "https://x.com/CesaltinoFelix"}
 ]
 
-const Socials = ()=> {
-    return <div>
+const Socials = ({containerStyles, iconStyles})=> {
+    return (
+    <div className={containerStyles}>
         {socials.map((social, index)=>{
-            return 
+            return<Link key={index} href={social.path} className={iconStyles} target="_blank">
+                    {social.icon}
+                </Link>
         })}
     </div>
+    )
 }
 
 export default Socials
